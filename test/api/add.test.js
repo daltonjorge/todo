d8a06b8062ee960923593d5890658e6380fc6955
Dalton Jorge <daltonjorge@gmail.com>
2019-09-25 08:08:10 -0300

    fix: 🐛 add and remove tests

--- test/api/add.test.js
+++ test/api/add.test.js
@@ -1,18 +1,20 @@
-const api = require('../../src/api/taskAPI')
+const {listTasks, addTask} = require('../../src/api/taskAPI')
+// import { listTasks, addTask } from '../../src/api/taskAPI'
 
 describe('Adding tasks', () => {
 
   it('add a task to empty list should return 1 item', () => {
     const tasks = []
     const expected = [{done: false, task: 'My task #1'}]
-    const tasks_updated = api.addTask(tasks, false, 'My task #1')
-    expect(tasks_updated).toEqual(expected);
+    // const tasks_updated = api.addTask(tasks, false, 'My task #1')
+    expect(listTasks(tasks)).toEqual([])
+    expect(addTask(tasks, false, 'My task #1')).toEqual(expected)
   });
 
   it('add a task to null list should return 1 item', () => {
     const tasks = null
-    const expected = [{done: false, task: 'My task #1'}]
-    const tasks_updated = api.addTask(tasks, false, 'My task #1')
+    const expected = [{done: true, task: 'My task #1'}]
+    const tasks_updated = addTask(tasks, true, 'My task #1')
     expect(tasks_updated).toEqual(expected);
   });
 });
